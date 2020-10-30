fe3c27bcdbb5e39176d505c3f34ae16722f31e7e
orgrinrt <ollipekka.toimela@eternal-stream.net>
2020-10-31 01:28:32 +0200

    Added some rudimentary threading to Chonker. As I'm not 100% familiar with the consequences of this, we should mark this commit and recall to a prior one if things get poopy

--- Chunky.Shared/Chonker.cs
+++ Chunky.Shared/Chonker.cs
@@ -1,17 +1,22 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Threading;
 
 namespace Chunky.Shared
 {
     public class Chonker
     {
-        private ColorRgba32bit[,] _map;
+        private readonly ColorRgba32bit[,] _map;
         private int _chunkWidth;
         private int _chunkHeight;
         private short _chunkCountX;
         private short _chunkCountY;
+
+        private int _totalChunkCount;
+        private ChunkData[,] _processedChunks;
 
         public Chonker(ColorRgba32bit[,] map, int chunkWidth, int chunkHeight)
         {
@@ -50,89 +55,95 @@ namespace Chunky.Shared
             SolveChunkSize();
         }
         
-        /*
-        public Chonker(short totalChunkCount)
-        {
-            SolveAll();
-        }
-        */
-        
         public ChunkData[,] GenerateChunks()
-        {
+        {/*
             Console.WriteLine("CHUNKWIDTH: " + _chunkWidth);
             Console.WriteLine("CHUNKHEIGHT: " + _chunkHeight);
             Console.WriteLine("CHUNK COUNTX " + _chunkCountX);
-            Console.WriteLine("CHUNK COUNTY: " + _chunkCountY);
-            ChunkData[,] result = new ChunkData[_chunkCountX, _chunkCountY];
-            
-            short originX, originY;
-
-            // TODO: We should definitely chonk-a-bonk each chunk in a separate task for multithreading experience
+            Console.WriteLine("CHUNK COUNTY: " + _chunkCountY);*/
+            _processedChunks = new ChunkData[_chunkCountX, _chunkCountY];
+            _totalChunkCount = _chunkCountX * _chunkCountY;
+            int _processedCount = 0;
+            object countLock = new object();
             
             for (int x = 0; x < _chunkCountX; x++)
             {
                 for (int y = 0; y < _chunkCountY; y++)
                 {
-                    originX = (short) Math.Round((double) (x * _chunkWidth));
-                    originY = (short) Math.Round((double) (y * _chunkHeight));
-
-                    Bitmap bitmap = new Bitmap(_chunkWidth, _chunkHeight, PixelFormat.Format32bppRgb);
-                    Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                    // process each chunk in its own thread
+                    // we know it'll be fine because each operation only adds an element to a unique index in a pre-allocated array
+                    int x1 = x;
+                    int y1 = y;
                     
-                    BitmapData data = bitmap.LockBits(
-                        rect, 
-                        ImageLockMode.ReadWrite,
-                        PixelFormat.Format32bppRgb);
-                    
-                    IntPtr ptr = data.Scan0;
-
-                    int bytes = data.Stride * data.Height;
-                    byte[] rgbValues = new byte[bytes];
-                    int index = 0;
-                    
-                    System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-                    for (int cy = originY; cy < originY + _chunkHeight; cy++)
+                    Thread thread = new Thread(() =>
                     {
-                        for (int cx = originX; cx < originX + _chunkWidth; cx++)
-                        {
-                            //Console.WriteLine("X: " + ((cy - originY) + 1) + ", Y: " + (cx - originX));
-                            for (int i = 0; i < 4; i++)
-                            {
-                                if (index > bytes - 1 ||
-                                    cx > _map.GetLength(0) - 1 ||
-                                    cy > _map.GetLength(1) - 1) break;
-
-                                switch (i)
-                                {
-                                    case 0:
-                                        rgbValues[index] = _map[cx, cy].B;
-                                        break;
-                                    case 1:
-                                        rgbValues[index] = _map[cx, cy].G;
-                                        break;
-                                    case 2:
-                                    rgbValues[index] = _map[cx, cy].R;
-                                        break;
-                                    case 3:
-                                        rgbValues[index] = _map[cx, cy].A;
-                                        break;
-                                }
-                                
-                                index++;
-                            }
-                        }
-                    }
-                    
-                    System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-                    
-                    bitmap.UnlockBits(data);
-
-                    result[x, y] = new ChunkData(bitmap, x, y);
+                        _processedChunks[x1, y1] = GenerateChunk(x1, y1);
+                        lock (countLock) _processedCount++;
+                    });
+                    thread.Start();
                 }
             }
+            
+            while (_processedCount < _totalChunkCount) { }
 
-            return result;
+            return _processedChunks;
+        }
+
+        private ChunkData GenerateChunk(int x, int y)
+        {
+            short originX = (short) Math.Round((double) (x * _chunkWidth));
+            short originY = (short) Math.Round((double) (y * _chunkHeight));
+
+            Bitmap bitmap = new Bitmap(_chunkWidth, _chunkHeight, PixelFormat.Format32bppRgb);
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            
+            BitmapData data = bitmap.LockBits(
+                rect, 
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb);
+            
+            IntPtr ptr = data.Scan0;
+
+            int bytes = data.Stride * data.Height;
+            byte[] rgbValues = new byte[bytes];
+            int index = 0;
+            
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            for (int cy = originY; cy < originY + _chunkHeight; cy++)
+            {
+                for (int cx = originX; cx < originX + _chunkWidth; cx++)
+                {
+                    //Console.WriteLine("X: " + ((cy - originY) + 1) + ", Y: " + (cx - originX));
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (index > bytes - 1 ||
+                            cx > _map.GetLength(0) - 1 ||
+                            cy > _map.GetLength(1) - 1) break;
+
+                        switch (i)
+                        {
+                            case 0:
+                                rgbValues[index] = _map[cx, cy].B;
+                                break;
+                            case 1:
+                                rgbValues[index] = _map[cx, cy].G;
+                                break;
+                            case 2:
+                            rgbValues[index] = _map[cx, cy].R;
+                                break;
+                            case 3:
+                                rgbValues[index] = _map[cx, cy].A;
+                                break;
+                        }
+                        
+                        index++;
+                    }
+                }
+            }
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            bitmap.UnlockBits(data);
+            return new ChunkData(bitmap, x, y);
         }
 
         private void SolveChunkSize()
