41d09eac596c56aa769458e2da6588ca3966833d
orgrinrt <ollipekka.toimela@eternal-stream.net>
2020-10-31 00:44:49 +0200

    Cleaned up the cli app and made it use the controller

--- Chunky/Program.cs
+++ Chunky/Program.cs
@@ -11,37 +11,17 @@ namespace Chunky
     {
         static void Main(string[] args)
         {
-            string fileName = "colorscene.png";
+            string fileName = "testmap.png";
             string assetsDir = Path.Combine(Utils.SolveAssemblyRootDir(Assembly.GetCallingAssembly()), "Assets");
             string testMapPath = Path.Combine(assetsDir, fileName);
             Console.WriteLine(testMapPath);
-            MapLoader loader = new MapLoader(testMapPath);
-            Chonker chonker = new Chonker(loader.Map, 410, 225);
-            SaveResultToDisk(chonker.GenerateChunks(), Path.Combine(assetsDir, "Results"), Utils.SolveNameFromFileName(fileName), loader.OriginalBitmap);
-        }
-
-        private static void SaveResultToDisk(ChunkData[,] result, string targetDir, string name, Bitmap original)
-        {
-            string path = Path.Combine(targetDir, name);
             
-            Directory.CreateDirectory(path);
-            ChunkData chunk;
-
-            for (int x = 0; x < result.GetLength(0); x++)
+            ChunkyController controller = new ChunkyController(new ChunkyConfig(testMapPath)
             {
-                for (int y = 0; y < result.GetLength(1); y++)
-                {
-                    chunk = result[x,y];
-                        
-                    chunk.Bitmap.Save(Path.Combine(path, name + "-" + chunk.X + "-" + chunk.Y + ".png"), ImageFormat.Png);
-                    //Console.WriteLine(chunk.Bitmap.GetPixel(0 + offset, 75));
-                }
-            }
-
-            Reconstructor reconstructor = new Reconstructor(result, name, (short)original.Width, (short)original.Height);
-            
-            reconstructor.ReconstructAndCompare(original, path);
-            //reconstructor.Reconstruct(targetDir);
+                ChunkWidth = 256,
+                ChunkHeight = 256
+            });
+            controller.Chunkify();
         }
 
         private static void ParseArgs(string[] args)
