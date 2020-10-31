df9f7de58cdfdf8645725229bc5caf54fb7a3365
orgrinrt <ollipekka.toimela@eternal-stream.net>
2020-10-31 10:56:10 +0200

    Updated program to parse args and make use of the cli helper we made

--- Chunky/Program.cs
+++ Chunky/Program.cs
@@ -9,13 +9,21 @@ namespace Chunky
 {
     class Program
     {
+        private static string _sourcePath;
+        private static ChunkyConfig32bit _currConfig;
+        private static CommandLineController _cli = new CommandLineController(
+            "Chunky", 
+            "A simple program to turn images into chunks.");
         static void Main(string[] args)
         {
+            ParseArgs(args);
+            Console.ReadLine();
+            /*
             string fileName = "colorscene.png";
             string assetsDir = Path.Combine(Utils.SolveAssemblyRootDir(Assembly.GetCallingAssembly()), "Assets");
             string testMapPath = Path.Combine(assetsDir, fileName);
             Console.WriteLine(testMapPath);
-            
+            */
             /*
             ChunkyProcessor processor = new ChunkyProcessor(new ChunkyConfig32bit(
                 testMapPath,
@@ -26,7 +34,7 @@ namespace Chunky
                 true));
             processor.Chunkify();
             */
-            
+            /*
             ChunkyBatchProcessor batcher = new ChunkyBatchProcessor(new ChunkyConfig32bit(
                 assetsDir,
                 (short)10,
@@ -35,14 +43,24 @@ namespace Chunky
                 true,
                 true));
             batcher.Chunkify();
-            
+            */
         }
 
         private static void ParseArgs(string[] args)
         {
-            foreach (string arg in args)
+            for (int i = 0; i < args.Length; i += 2)
             {
-                
+                if (_cli.Commands.ContainsKey(args[i]))
+                {
+                    if (args.Length > i + 2)
+                    {
+                        _cli.Actions[_cli.Commands[args[i]]](args[i + 1]);
+                    }
+                    else
+                    {
+                        _cli.Actions[_cli.Commands[args[i]]]("");
+                    }
+                }
             }
         }
     }
