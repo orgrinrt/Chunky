69df8e379e05c01fb93bbe2a53de3c917a02c598
orgrinrt <ollipekka.toimela@eternal-stream.net>
2020-10-31 06:00:38 +0200

    Updated project file and Chunky cli program (currently just to do a batch operation on files within "Assets")

--- Chunky/Program.cs
+++ Chunky/Program.cs
@@ -16,14 +16,26 @@ namespace Chunky
             string testMapPath = Path.Combine(assetsDir, fileName);
             Console.WriteLine(testMapPath);
             
+            /*
             ChunkyController controller = new ChunkyController(new ChunkyConfig32bit(
                 testMapPath,
-                256,
-                256,
+                (short)10,
+                (short)10,
                 false,
-                false,
-                false));
+                true,
+                true));
             controller.Chunkify();
+            */
+            
+            ChunkyBatchController batcher = new ChunkyBatchController(new ChunkyConfig32bit(
+                assetsDir,
+                (short)10,
+                (short)10,
+                false,
+                true,
+                true));
+            batcher.Chunkify();
+            
         }
 
         private static void ParseArgs(string[] args)
